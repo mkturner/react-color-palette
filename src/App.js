b0b5e633e73372c4404298e614af4977e146383d
Marvin T <contact@marvinkturner.com>
2021-03-03 23:58:10 -0500

    feat: add structure to app component

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
+
+import Values from 'values.js';
+
+function App() {
+  // set up state hooks to use later
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  // handleSubmit function
+  const handleSubmit = (e) => {
+    // prevent default page refresh
+    e.preventDefault();
+    // check contents
+    console.log(e);
+  };
+
+  return (
+    <>
+      <section className='container'>
+        <h1>Color Generator</h1>
+        <form onSubmit={handleSubmit}>
+          <input type='text' value={color} />
+        </form>
+      </section>
+      <section className='colors'>
+        <h3>List goes here.</h3>
+      </section>
+    </>
+  );
+}
+
+export default App;
