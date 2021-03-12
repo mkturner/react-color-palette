0a19f12becdc9ab93ca523b79fae740d491df467
Marvin T <contact@marvinkturner.com>
2021-03-11 23:56:41 -0500

    feat: dismiss clipboard notification after 3s

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -14,13 +14,33 @@ const SingleColor = ({ rgb, weight, index, hex }) => {
   // const hexCode = rgbToHex(...rgb);
   const hexValue = `#${hex}`;
 
+  // set 3s timer to clear 'copied to clipboard' notification
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [alert]);
+
   return (
     <article
       className={`color ${index > 10 && 'color-light'}`}
       style={{ backgroundColor: `rgb(${bgColor})` }}
+      onClick={() => {
+        // when clicking a color, copy hex value to clipboard and notify user
+        navigator.clipboard.writeText(hexValue);
+        setAlert(true);
+      }}
     >
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>{hexValue}</p>
+
+      {
+        // if alert is true, will return <p>
+        alert && <p className='alert'>Copied to Clipboard</p>
+      }
     </article>
   );
 };
